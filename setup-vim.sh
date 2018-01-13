b1ca0d63697ea09b656db1b759285b59db6df321
ViktorBarzin <vbarzin@gmail.com>
2018-01-13 21:32:03 +0000

    add shell setup

--- setup-vim.sh
+++ setup-vim.sh
@@ -10,7 +10,6 @@ rm /usr/share/vim/vimrc
 wget https://raw.githubusercontent.com/ViktorBarzin/dot_files/master/.vimrc -O ~/.vimrc;
 # mkdir -p ~/.vim/colors && cd ~/.vim/colors;
 # wget -O wombat256mod.vim http://www.vim.org/scripts/download_script.php?src_id=13400;
-git clone https://github.com/ViktorBarzin/dot_files.git
 rm -rf ~/.vim
 mv dot_files/vim-plugins ~/.vim
 rm -rf dot_files
