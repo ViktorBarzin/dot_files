39792c25f1452f78e018db84541a8fb2996bbfdb
ViktorBarzin <vbarzin@gmail.com>
2018-01-15 21:42:57 +0000

    add some vim stuff

--- setup-vim.sh
+++ setup-vim.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
-apt-get update >> /dev/null;
-apt-get install git -y;
-apt-get install vim-gnome -y;
 
-# Remove default vimrc
-rm /usr/share/vim/vimrc
+# Add vim 8 repository
+sudo add-apt-repository ppa:jonathonf/vim
+apt-get update >> /dev/null;
+apt-get install vim -y;
+
+rm -rf ~/.vim
+# To uninstall and return to stock
+# sudo apt install ppa-purge && sudo ppa-purge ppa:jonathonf/vim
 
 # Download color scheme
 wget https://raw.githubusercontent.com/ViktorBarzin/dot_files/master/.vimrc -O ~/.vimrc;
+# Move plugins to plugins folder
+mv vim-plugins ~/.vim
+
+# Update system default link to vimrc
+rm /usr/share/vim/vimrc
+ln -s "/home/$USER/.vimrc" /usr/share/vim/vimrc
+
 # mkdir -p ~/.vim/colors && cd ~/.vim/colors;
 # wget -O wombat256mod.vim http://www.vim.org/scripts/download_script.php?src_id=13400;
-rm -rf ~/.vim
-mv dot_files/vim-plugins ~/.vim
-rm -rf dot_files
 echo 'Done!'
 echo
 echo 'You may use your newly setup vim!'
