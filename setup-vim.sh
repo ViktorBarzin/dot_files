8793bad4e4d2d77b87e7eaa58e0d335746849145
ViktorBarzin <vbarzin@gmail.com>
2018-01-15 21:53:39 +0000

    add vim install script updates

--- setup-vim.sh
+++ setup-vim.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+echo 'Installing vim...'
 
-# Add vim 8 repository
-sudo add-apt-repository ppa:jonathonf/vim
+# Add vim 8 repository and install vim8.0
+printf '\n' | sudo add-apt-repository ppa:jonathonf/vim
 apt-get update >> /dev/null;
 apt-get install vim -y;
 
+# Remove default vim config directory
 rm -rf ~/.vim
 # To uninstall and return to stock
 # sudo apt install ppa-purge && sudo ppa-purge ppa:jonathonf/vim
 
-# Download color scheme
+# Download color scheme - ne bash
 wget https://raw.githubusercontent.com/ViktorBarzin/dot_files/master/.vimrc -O ~/.vimrc;
 # Move plugins to plugins folder
 mv vim-plugins ~/.vim
